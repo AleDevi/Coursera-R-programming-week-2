63a2c473b209c3bab78cf6308a4f9b3dc9fd200b
AleDevi <alessandro.devigili@gmail.com>
2020-08-03 11:20:35 +0200

    The second function for the Coursera week 2 course

---
+++ complete.R
@@ -0,0 +1,8 @@
+complete<- function(directory, id=1:332){
+  setwd(paste("C:/Users/Color&Sound/Desktop/Coursera/week2/",directory,sep=""))
+  files<-list.files()
+  fulldata<-data.frame()
+  for(i in id){
+    
+  }
+}
